9731536aa3dec27aab25f06e658b8b56ccfcced8
jorge <jorge mauro goncalves>
2019-08-07 00:18:20 -0300

    api & redux inicio

---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+export const config = {
+    urlData:"http://www.omdbapi.com/",
+    urlImg:"http://img.omdbapi.com/",
+    key:'key',
+    options:'language=language%3Dpt-BR'
+};
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { store, history } from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <ConnectedRouter history={history}>
+        <Switch>
+            <Route path="/" component={App} />
+        </Switch>
+    </ConnectedRouter>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
